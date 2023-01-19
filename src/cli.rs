926a82e8e6a4eac4b158bb143d09befb45f51fbf
Filip Tibell <filip.tibell@gmail.com>
2023-01-19 13:18:15 -0500

    Implement downloading of type definition files in CLI

--- src/cli.rs
+++ src/cli.rs
@@ -3,18 +3,23 @@ use std::{
     path::{PathBuf, MAIN_SEPARATOR},
 };
 
-use clap::Parser;
+use clap::{CommandFactory, Parser};
 use mlua::{Lua, MultiValue, Result, ToLua};
 
-use crate::lune::{fs::LuneFs, json::LuneJson, process::LuneProcess};
+use crate::{
+    lune::{fs::LuneFs, json::LuneJson, process::LuneProcess},
+    utils::GithubClient,
+};
 
 /// Lune CLI
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Default)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
     /// Path to the file to run, or the name
     /// of a luau file in a lune directory
-    script_path: String,
+    ///
+    /// Can be omitted when downloading type definitions
+    script_path: Option<String>,
     /// Arguments to pass to the file as vararg (...)
     script_args: Vec<String>,
     /// Pass this flag to download the Selene type
@@ -27,17 +32,6 @@ pub struct Cli {
     download_luau_types: bool,
 }
 
-impl Default for Cli {
-    fn default() -> Self {
-        Self {
-            script_path: "".to_string(),
-            script_args: vec![],
-            download_selene_types: false,
-            download_luau_types: false,
-        }
-    }
-}
-
 impl Cli {
     #[allow(dead_code)]
     pub fn from_path<S>(path: S) -> Self
@@ -45,7 +39,7 @@ impl Cli {
         S: Into<String>,
     {
         Self {
-            script_path: path.into(),
+            script_path: Some(path.into()),
             ..Default::default()
         }
     }
@@ -57,15 +51,52 @@ impl Cli {
         A: Into<Vec<String>>,
     {
         Self {
-            script_path: path.into(),
+            script_path: Some(path.into()),
             script_args: args.into(),
             ..Default::default()
         }
     }
 
     pub async fn run(self) -> Result<()> {
+        // Download definition files, if wanted
+        let download_types_requested = self.download_selene_types || self.download_luau_types;
+        if download_types_requested {
+            let client = GithubClient::new().map_err(mlua::Error::external)?;
+            let release = client
+                .fetch_release_for_this_version()
+                .await
+                .map_err(mlua::Error::external)?;
+            if self.download_selene_types {
+                println!("Downloading Selene type definitions...");
+                client
+                    .fetch_release_asset(&release, "lune.yml")
+                    .await
+                    .map_err(mlua::Error::external)?;
+            }
+            if self.download_luau_types {
+                println!("Downloading Luau type definitions...");
+                client
+                    .fetch_release_asset(&release, "luneTypes.d.luau")
+                    .await
+                    .map_err(mlua::Error::external)?;
+            }
+        }
+        if self.script_path.is_none() {
+            if !download_types_requested {
+                // HACK: We know that we didn't get any arguments here but since
+                // script_path is optional clap will not error on its own, to fix
+                // we will duplicate the cli command and make arguments required,
+                // which will then fail and print out the normal help message
+                let cmd = Cli::command();
+                cmd.arg_required_else_help(true).get_matches();
+            } else {
+                // Only downloading types without running a script is completely
+                // fine, and we should just exit the program normally afterwards
+                return Ok(());
+            }
+        }
         // Parse and read the wanted file
-        let file_path = find_parse_file_path(&self.script_path)?;
+        let file_path = find_parse_file_path(&self.script_path.unwrap())?;
         let file_contents = read_to_string(file_path)?;
         // Create a new lua state and add in all lune globals
         let lua = Lua::new();
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,133 @@
+use std::env::current_dir;
+
+use anyhow::{bail, Context, Result};
+use reqwest::{
+    header::{HeaderMap, HeaderValue},
+    Client,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Deserialize, Serialize)]
+pub struct GithubReleaseAsset {
+    id: u64,
+    url: String,
+    name: Option<String>,
+    label: Option<String>,
+    content_type: String,
+    size: u64,
+}
+
+#[derive(Clone, Deserialize, Serialize)]
+pub struct GithubRelease {
+    id: u64,
+    url: String,
+    tag_name: String,
+    name: Option<String>,
+    body: Option<String>,
+    draft: bool,
+    prerelease: bool,
+    assets: Vec<GithubReleaseAsset>,
+}
+
+pub struct GithubClient {
+    client: Client,
+    github_owner: String,
+    github_repo: String,
+}
+
+impl GithubClient {
+    pub fn new() -> Result<Self> {
+        let (github_owner, github_repo) = env!("CARGO_PKG_REPOSITORY")
+            .strip_prefix("https://github.com/")
+            .unwrap()
+            .split_once('/')
+            .unwrap();
+        let mut headers = HeaderMap::new();
+        headers.insert(
+            "User-Agent",
+            HeaderValue::from_str(&format!("{}-{}-cli", github_owner, github_repo))?,
+        );
+        headers.insert(
+            "Accept",
+            HeaderValue::from_static("application/vnd.github+json"),
+        );
+        headers.insert(
+            "X-GitHub-Api-Version",
+            HeaderValue::from_static("2022-11-28"),
+        );
+        let client = Client::builder().default_headers(headers).build()?;
+        Ok(Self {
+            client,
+            github_owner: github_owner.to_string(),
+            github_repo: github_repo.to_string(),
+        })
+    }
+
+    pub async fn fetch_releases(&self) -> Result<Vec<GithubRelease>> {
+        let release_api_url = format!(
+            "https://api.github.com/repos/{}/{}/releases",
+            &self.github_owner, &self.github_repo
+        );
+        let response_bytes = self
+            .client
+            .get(release_api_url)
+            .send()
+            .await
+            .context("Failed to send releases request")?
+            .bytes()
+            .await
+            .context("Failed to get releases response bytes")?;
+        let response_body: Vec<GithubRelease> = serde_json::from_slice(&response_bytes)?;
+        Ok(response_body)
+    }
+
+    pub async fn fetch_release_for_this_version(&self) -> Result<GithubRelease> {
+        let release_version_tag = format!("v{}", env!("CARGO_PKG_VERSION"));
+        let all_releases = self.fetch_releases().await?;
+        all_releases
+            .iter()
+            .find(|release| release.tag_name == release_version_tag)
+            .map(|release| release.to_owned())
+            .with_context(|| format!("Failed to find release for version {}", release_version_tag))
+    }
+
+    pub async fn fetch_release_asset(
+        &self,
+        release: &GithubRelease,
+        asset_name: &str,
+    ) -> Result<()> {
+        if let Some(asset) = release
+            .assets
+            .iter()
+            .find(|asset| matches!(&asset.name, Some(name) if name == asset_name))
+        {
+            let file_path = current_dir()?.join(asset_name);
+            let file_bytes = self
+                .client
+                .get(&asset.url)
+                .header("Accept", "application/octet-stream")
+                .send()
+                .await
+                .context("Failed to send asset download request")?
+                .bytes()
+                .await
+                .context("Failed to get asset download response bytes")?;
+            tokio::fs::write(&file_path, &file_bytes)
+                .await
+                .with_context(|| {
+                    format!("Failed to write file at path '{}'", &file_path.display())
+                })?;
+        } else {
+            bail!(
+                "Failed to find release asset '{}' for release '{}'",
+                asset_name,
+                &release.tag_name
+            )
+        }
+        Ok(())
+    }
+}
+
 pub fn pretty_print_luau_error(e: &mlua::Error) {
     match e {
         mlua::Error::RuntimeError(e) => {
