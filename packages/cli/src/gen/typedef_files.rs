0db32ad4c4f83855f1b700c0e6f43fac794e224b
Filip Tibell <filip.tibell@gmail.com>
2023-05-14 22:01:44 +0200

    Actually, lets just not process the typedefs at all

--- packages/cli/src/gen/typedef_files.rs
+++ packages/cli/src/gen/typedef_files.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, fmt::Write, path::PathBuf};
+use std::{collections::HashMap, path::PathBuf};
 
 use anyhow::{Context, Result};
 use directories::UserDirs;
@@ -6,13 +6,9 @@ use directories::UserDirs;
 use futures_util::future::try_join_all;
 use tokio::fs::{create_dir_all, write};
 
-use super::definitions::{DefinitionsItem, DefinitionsTree};
-
-const GENERATED_COMMENT_TAG: &str = "--!strict";
-
 #[allow(clippy::too_many_lines)]
 pub async fn generate_from_type_definitions(
-    api_reference: HashMap<String, DefinitionsTree>,
+    typedef_files: HashMap<String, Vec<u8>>,
 ) -> Result<HashMap<String, PathBuf>> {
     let mut dirs_to_write = Vec::new();
     let mut files_to_write = Vec::new();
@@ -25,19 +21,11 @@ pub async fn generate_from_type_definitions(
         .join(env!("CARGO_PKG_VERSION"));
     dirs_to_write.push(cache_dir.clone());
     // Make typedef files
-    for (category_name, category_tree) in api_reference {
+    for (builtin_name, builtin_typedef) in typedef_files {
         let path = cache_dir
-            .join(category_name.to_ascii_lowercase())
+            .join(builtin_name.to_ascii_lowercase())
             .with_extension("luau");
-        let mut contents = String::new();
-        write!(
-            contents,
-            "{GENERATED_COMMENT_TAG}\n-- @lune/{} {}\n",
-            category_name.to_lowercase(),
-            env!("CARGO_PKG_VERSION")
-        )?;
-        write_tree(&mut contents, category_name.to_string(), category_tree)?;
-        files_to_write.push((category_name.to_lowercase(), path, contents));
+        files_to_write.push((builtin_name.to_lowercase(), path, builtin_typedef));
     }
     // Write all dirs and files only when we know generation was successful
     let futs_dirs = dirs_to_write
@@ -54,114 +42,4 @@ pub async fn generate_from_type_definitions(
         .drain(..)
         .map(|(name, path, _)| (name, path))
         .collect::<HashMap<_, _>>())
-}
-
-fn make_return_table_item(item: &DefinitionsItem) -> Result<String> {
-    let mut description = String::new();
-    if let Some(desc) = item.children().iter().find(|child| child.is_description()) {
-        write!(description, "\n{}\n", desc.get_value().unwrap().trim())?;
-        for tag in item.children().iter().filter(|child| child.is_tag()) {
-            let tag_name = tag.get_name().unwrap();
-            if tag_name == "param" {
-                write!(
-                    description,
-                    "\n@param {} {}",
-                    tag.get_meta().unwrap(),
-                    tag.get_value().unwrap()
-                )?;
-            } else if tag_name == "return" {
-                write!(description, "\n@return {}", tag.get_value().unwrap())?;
-            }
-        }
-    }
-
-    let mut contents = String::new();
-    if item.is_function() {
-        let args = item
-            .args()
-            .iter()
-            .map(|arg| format!("{}: {}", arg.name.trim(), arg.typedef.trim()))
-            .collect::<Vec<_>>()
-            .join(", ");
-        // HACK: We should probably handle vararg and generics properly but this works for now...
-        let args = args
-            .replace("_: ...any", "...: any")
-            .replace("_: T...", "...: any")
-            .replace("(T...)", "(...any)");
-        write!(contents, "function({args})")?;
-        write!(contents, "\n\treturn nil :: any")?;
-        write!(contents, "\nend,")?;
-    } else if item.is_property() {
-        write!(contents, "(nil :: any) :: {},", item.get_type().unwrap())?;
-    }
-
-    Ok(format!(
-        "\n--[=[{}\n]=]\n{} = {}",
-        description.trim_end().replace('\n', "\n\t"),
-        item.get_name().unwrap_or("_"),
-        contents
-    ))
-}
-
-fn write_tree(contents: &mut String, name: String, root: DefinitionsTree) -> Result<()> {
-    let main = root
-        .children()
-        .iter()
-        .find(|c| matches!(c.get_name(), Some(s) if s.to_lowercase() == name.to_lowercase()))
-        .expect("Failed to find main export for generating typedef file");
-
-    let mut description = String::new();
-    if let Some(desc) = main.children().iter().find(|child| child.is_description()) {
-        write!(description, "\n{}\n", desc.get_value().unwrap().trim())?;
-    }
-
-    let children = root
-        .children()
-        .iter()
-        .filter(|child| child != &main)
-        .collect::<Vec<_>>();
-    for child in children {
-        if child.is_type() || child.is_table() || child.is_function() || child.is_property() {
-            let mut child_description = String::new();
-            if let Some(desc) = child.children().iter().find(|child| child.is_description()) {
-                write!(
-                    child_description,
-                    "\n{}\n",
-                    desc.get_value().unwrap().trim()
-                )?;
-                write!(
-                    contents,
-                    "\n--[=[{}\n]=]",
-                    child_description.trim_end().replace('\n', "\n\t"),
-                )?;
-            }
-            if child.is_exported() {
-                write!(contents, "\nexport ")?;
-            }
-            writeln!(
-                contents,
-                "type {} = {}",
-                child.get_name().unwrap(),
-                child.get_type().unwrap()
-            )?;
-        }
-    }
-
-    let mut ret_table = String::new();
-    for child in main
-        .children()
-        .iter()
-        .filter(|child| child.is_function() || child.is_property())
-    {
-        write!(ret_table, "{}", make_return_table_item(child)?)?;
-    }
-
-    write!(
-        contents,
-        "\n--[=[{}\n]=]\nreturn {{\n{}\n}}\n",
-        description.trim_end().replace('\n', "\n\t"),
-        ret_table.trim_end().replace('\n', "\n\t")
-    )?;
-
-    Ok(())
 }
