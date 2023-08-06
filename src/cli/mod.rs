bd594e6f07cc7caca00944ec21002f9f049b9847
Compey <hi@devcomp.xyz>
2023-08-06 16:18:37 +0530

    feat(repl): initial implementation

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,7 +1,13 @@
-use std::{fmt::Write as _, process::ExitCode};
+use std::{
+    fmt::Write as _,
+    io::ErrorKind,
+    path::PathBuf,
+    process::{exit, ExitCode},
+};
 
 use anyhow::{Context, Result};
 use clap::{CommandFactory, Parser};
+use rustyline::{error::ReadlineError, DefaultEditor};
 
 use lune::Lune;
 use tokio::{
@@ -148,10 +154,92 @@ impl Cli {
             }
             // HACK: We know that we didn't get any arguments here but since
             // script_path is optional clap will not error on its own, to fix
-            // we will duplicate the cli command and make arguments required,
-            // which will then fail and print out the normal help message
+            // we will duplicate the CLI command and fetch the version of
+            // lune to display
             let cmd = Cli::command();
-            cmd.arg_required_else_help(true).get_matches();
+            let lune_version = cmd.get_version();
+
+            // The version is mandatory and will always exist
+            println!("Lune v{}", lune_version.unwrap());
+
+            let lune_instance = Lune::new();
+
+            let mut repl = DefaultEditor::new()?;
+
+            match repl.load_history(&(|| -> PathBuf {
+                let dir_opt = home::home_dir();
+
+                if let Some(dir) = dir_opt {
+                    dir.join(".lune_history")
+                } else {
+                    eprintln!("Failed to find user home directory, abort!");
+                    // Doesn't feel right to exit directly with a exit code of 1
+                    // Lmk if there is a better way of doing this
+                    exit(1);
+                }
+            })()) {
+                Ok(_) => (),
+                Err(err) => {
+                    match err {
+                        err => {
+                            if let ReadlineError::Io(io_err) = err {
+                                if io_err.kind() == ErrorKind::NotFound {
+                                    std::fs::write(
+                                        // We know for sure that the home dir already exists
+                                        home::home_dir().unwrap().join(".lune_history"),
+                                        String::new(),
+                                    )?;
+                                }
+                            }
+                        }
+                    }
+
+                    eprintln!("WARN: Failed to load REPL history")
+                }
+            };
+
+            let mut interrupt_counter = 0u32;
+
+            loop {
+                let mut source_code = String::new();
+
+                match repl.readline("> ") {
+                    Ok(code) => {
+                        source_code = code;
+
+                        // If source code eval was requested, we reset the counter
+                        interrupt_counter = 0;
+                    }
+
+                    Err(ReadlineError::Interrupted) => {
+                        // HACK: We actually want the user to do ^C twice to exit,
+                        // but the user would need to ^C one more time even after
+                        // the check passes, so we check for 1 instead of 2
+                        if interrupt_counter != 1 {
+                            println!("Interrupt: ^C again to exit");
+
+                            // Increment the counter
+                            interrupt_counter += 1;
+                        } else {
+                            break;
+                        }
+                    }
+                    Err(ReadlineError::Eof) => break,
+                    Err(err) => {
+                        eprintln!("REPL ERROR: {}", err.to_string());
+                        break;
+                    }
+                };
+
+                let eval_result = lune_instance.run("REPL", source_code).await;
+
+                match eval_result {
+                    Ok(_) => (),
+                    Err(err) => eprintln!("{}", err),
+                }
+            }
+
+            return Ok(ExitCode::SUCCESS);
         }
         // Figure out if we should read from stdin or from a file,
         // reading from stdin is marked by passing a single "-"
