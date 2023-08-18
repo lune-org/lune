0255355553be211b0c309a879f93a7c6fb411de8
Filip Tibell <filip.tibell@gmail.com>
2023-08-17 21:37:09 -0500

    Methods take owned threads

--- src/lune/scheduler/impl_async.rs
+++ src/lune/scheduler/impl_async.rs
@@ -1,7 +1,7 @@
 use futures_util::Future;
 use mlua::prelude::*;
 
-use super::{traits::IntoLuaOwnedThread, SchedulerImpl};
+use super::SchedulerImpl;
 
 impl<'lua, 'fut> SchedulerImpl<'fut>
 where
@@ -10,7 +10,7 @@ where
     /**
         Schedules a plain future to run whenever the scheduler is available.
     */
-    pub fn schedule_future<F>(&self, fut: F)
+    pub fn schedule_future<F>(&'lua self, fut: F)
     where
         F: 'fut + Future<Output = ()>,
     {
@@ -24,15 +24,11 @@ where
     /**
         Schedules the given `thread` to run when the given `fut` completes.
     */
-    pub fn schedule_future_thread<T, R, F>(&'fut self, thread: T, fut: F) -> LuaResult<()>
+    pub fn schedule_future_thread<R, F>(&'lua self, thread: LuaOwnedThread, fut: F) -> LuaResult<()>
     where
-        T: IntoLuaOwnedThread,
         R: IntoLuaMulti<'fut>,
         F: 'fut + Future<Output = LuaResult<R>>,
     {
-        let thread = thread.into_owned_lua_thread(&self.lua)?;
-
-        // FIXME: We use self in the future below, so this doesn't compile... how to fix?
         self.schedule_future(async move {
             let rets = fut.await.expect("Failed to receive result");
             self.push_back(thread, rets)
