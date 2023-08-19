2762a43dbb1d6ca89e5e6edc567add5ca27e4db0
Filip Tibell <filip.tibell@gmail.com>
2023-08-19 17:23:40 -0500

    Add back _G table

--- src/lune/globals/mod.rs
+++ src/lune/globals/mod.rs
@@ -2,10 +2,12 @@ use mlua::prelude::*;
 
 use super::util::TableBuilder;
 
+mod g_table;
 mod require;
 
 pub fn inject_all(lua: &'static Lua) -> LuaResult<()> {
     let all = TableBuilder::new(lua)?
+        .with_value("_G", g_table::create(lua)?)?
         .with_value("require", require::create(lua)?)?
         .build_readonly()?;
 
