cb11b3f0e1f32f7e1147703c1e0f9742f25d7b09
Filip Tibell <filip.tibell@gmail.com>
2023-08-20 15:01:20 -0500

    Add back support for init files in require

--- src/lune/globals/require/path.rs
+++ src/lune/globals/require/path.rs
@@ -28,12 +28,35 @@ where
         return Ok(res);
     }
 
-    // 2. Try to require the path with an added "lua" extension
+    // 3. Try to require the path with an added "lua" extension
     let (lua_abs_path, lua_rel_path) = (
         append_extension(&abs_path, "lua"),
         append_extension(&rel_path, "lua"),
     );
     if let Ok(res) = require_inner(ctx, &lua_abs_path, &lua_rel_path).await {
+        return Ok(res);
+    }
+
+    // We didn't find any direct file paths, look
+    // for directories with "init" files in them...
+    let abs_init = abs_path.join("init");
+    let rel_init = rel_path.join("init");
+
+    // 4. Try to require the init path with an added "luau" extension
+    let (luau_abs_init, luau_rel_init) = (
+        append_extension(&abs_init, "luau"),
+        append_extension(&rel_init, "luau"),
+    );
+    if let Ok(res) = require_inner(ctx, &luau_abs_init, &luau_rel_init).await {
+        return Ok(res);
+    }
+
+    // 5. Try to require the init path with an added "lua" extension
+    let (lua_abs_init, lua_rel_init) = (
+        append_extension(&abs_init, "lua"),
+        append_extension(&rel_init, "lua"),
+    );
+    if let Ok(res) = require_inner(ctx, &lua_abs_init, &lua_rel_init).await {
         return Ok(res);
     }
 
