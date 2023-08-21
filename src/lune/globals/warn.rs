6df6a195ae88b7ac73c74326eb0472f397276189
Filip Tibell <filip.tibell@gmail.com>
2023-08-20 19:54:29 -0500

    Add back print and warn globals

---
+++ src/lune/globals/warn.rs
@@ -0,0 +1,21 @@
+use mlua::prelude::*;
+use tokio::io::{self, AsyncWriteExt};
+
+use crate::lune::{
+    scheduler::LuaSchedulerExt,
+    util::formatting::{format_label, pretty_format_multi_value},
+};
+
+pub fn create(lua: &'static Lua) -> LuaResult<impl IntoLua<'_>> {
+    lua.create_async_function(|_, args: LuaMultiValue| async move {
+        let formatted = format!(
+            "{}\n{}",
+            format_label("warn"),
+            pretty_format_multi_value(&args)?
+        );
+        let mut stdout = io::stderr();
+        stdout.write_all(formatted.as_bytes()).await?;
+        stdout.flush().await?;
+        Ok(())
+    })
+}
