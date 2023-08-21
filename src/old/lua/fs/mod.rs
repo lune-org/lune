c90d51ab340fa24dea8b822452cd5f98a2d9395e
Filip Tibell <filip.tibell@gmail.com>
2023-08-20 19:18:16 -0500

    Remove old lune lib

--- src/old/lua/fs/mod.rs
+++
@@ -1,7 +0,0 @@
-mod copy;
-mod metadata;
-mod options;
-
-pub use copy::copy;
-pub use metadata::FsMetadata;
-pub use options::FsWriteOptions;
--- src/old/lua/task/ext/mod.rs
+++
@@ -1,7 +0,0 @@
-mod async_ext;
-mod resume_ext;
-mod schedule_ext;
-
-pub use async_ext::TaskSchedulerAsyncExt;
-pub use resume_ext::TaskSchedulerResumeExt;
-pub use schedule_ext::TaskSchedulerScheduleExt;
