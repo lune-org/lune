ebdc8a62610c2caf179e39f236671fde5b9da4a6
Filip Tibell <filip.tibell@gmail.com>
2023-08-20 19:18:16 -0500

    Remove old lune lib

--- src/old/lua/serde/mod.rs
+++
@@ -1,5 +0,0 @@
-mod compress_decompress;
-mod encode_decode;
-
-pub use compress_decompress::{compress, decompress, CompressDecompressFormat};
-pub use encode_decode::{EncodeDecodeConfig, EncodeDecodeFormat};
