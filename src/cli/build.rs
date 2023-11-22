60870694936e06b1718b1af1660c2af592d9c49d
Erica Marigold <hi@devcomp.xyz>
2023-11-22 22:34:10 +0530

    fix(windows): write file differently for windows

--- src/cli/build.rs
+++ src/cli/build.rs
@@ -44,14 +44,18 @@ pub async fn build_standalone<T: AsRef<Path> + Into<PathBuf>>(
     patched_bin.append(&mut signature.clone());
 
     // Write the compiled binary to file
-    OpenOptions::new()
-        .write(true)
-        .create(true)
-        .mode(0o770)
-        .open(output_path)
-        .await?
-        .write_all(&patched_bin)
-        .await?;
+    if cfg!(unix) {
+        OpenOptions::new()
+            .write(true)
+            .create(true)
+            .mode(0o770)
+            .open(output_path)
+            .await?
+            .write_all(&patched_bin)
+            .await?;
+    } else if cfg!(windows) {
+        fs::write(output_path, &patched_bin).await?;
+    }
 
     Ok(ExitCode::SUCCESS)
 }
