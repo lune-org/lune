aa64b45677e9a80863335559dd4af004e5100e29
qwreey <me@qwreey.moe>
2024-08-25 14:37:31 +0000

    Move c-type export into c/mod.rs

--- crates/lune-std-ffi/src/c/c_type.rs
+++ crates/lune-std-ffi/src/c/c_type.rs
@@ -1,9 +1,6 @@
 #![allow(clippy::cargo_common_metadata)]
 
-use core::ffi::{
-    c_char, c_double, c_float, c_int, c_long, c_longlong, c_schar, c_short, c_uchar, c_uint,
-    c_ulong, c_ulonglong, c_ushort, c_void,
-};
+use core::ffi::c_void;
 
 use libffi::middle::{Cif, Type};
 use mlua::prelude::*;
@@ -12,7 +9,6 @@ use super::c_arr::CArr;
 use super::c_helper::get_ensured_size;
 use super::c_ptr::CPtr;
 use crate::ffi::ffi_helper::get_ptr_from_userdata;
-use crate::ffi::ffi_platform::CHAR_IS_SIGNED;
 // use libffi::raw::{ffi_cif, ffi_ptrarray_to_raw};
 
 pub struct CType {
@@ -116,123 +112,4 @@ impl LuaUserData for CType {
             Ok(name)
         });
     }
-}
-
-// export all default c-types
-#[allow(clippy::too_many_lines)]
-pub fn create_all_types(lua: &Lua) -> LuaResult<Vec<(&'static str, LuaValue)>> {
-    Ok(vec![
-        (
-            "int",
-            CType::new(
-                Type::c_int(),
-                Some(String::from("int")),
-                |data, ptr| {
-                    let value = match data {
-                        LuaValue::Integer(t) => t,
-                        _ => {
-                            return Err(LuaError::external(format!(
-                                "Integer expected, got {}",
-                                data.type_name()
-                            )))
-                        }
-                    } as c_int;
-                    unsafe {
-                        *(ptr.cast::<c_int>()) = value;
-                    }
-                    Ok(())
-                },
-                |lua: &Lua, ptr: *mut c_void| {
-                    let value = unsafe { (*ptr.cast::<c_int>()).into_lua(lua)? };
-                    Ok(value)
-                },
-            )?
-            .into_lua(lua)?,
-        ),
-        (
-            "long",
-            CType::new(
-                Type::c_long(),
-                Some(String::from("long")),
-                |data, ptr| {
-                    let value = match data {
-                        LuaValue::Integer(t) => t,
-                        _ => {
-                            return Err(LuaError::external(format!(
-                                "Integer expected, got {}",
-                                data.type_name()
-                            )))
-                        }
-                    } as c_long;
-                    unsafe {
-                        *(ptr.cast::<c_long>()) = value;
-                    }
-                    Ok(())
-                },
-                |lua: &Lua, ptr: *mut c_void| {
-                    let value = unsafe { (*ptr.cast::<c_long>()).into_lua(lua)? };
-                    Ok(value)
-                },
-            )?
-            .into_lua(lua)?,
-        ),
-        (
-            "longlong",
-            CType::new(
-                Type::c_longlong(),
-                Some(String::from("longlong")),
-                |data, ptr| {
-                    let value = match data {
-                        LuaValue::Integer(t) => t,
-                        _ => {
-                            return Err(LuaError::external(format!(
-                                "Integer expected, got {}",
-                                data.type_name()
-                            )))
-                        }
-                    } as c_longlong;
-                    unsafe {
-                        *(ptr.cast::<c_longlong>()) = value;
-                    }
-                    Ok(())
-                },
-                |lua: &Lua, ptr: *mut c_void| {
-                    let value = unsafe { (*ptr.cast::<c_longlong>()).into_lua(lua)? };
-                    Ok(value)
-                },
-            )?
-            .into_lua(lua)?,
-        ),
-        (
-            "char",
-            CType::new(
-                if CHAR_IS_SIGNED {
-                    Type::c_schar()
-                } else {
-                    Type::c_uchar()
-                },
-                Some(String::from("char")),
-                |data, ptr| {
-                    let value = match data {
-                        LuaValue::Integer(t) => t,
-                        _ => {
-                            return Err(LuaError::external(format!(
-                                "Integer expected, got {}",
-                                data.type_name()
-                            )))
-                        }
-                    } as c_char;
-                    unsafe {
-                        *(ptr.cast::<c_char>()) = value;
-                    }
-                    Ok(())
-                },
-                |lua: &Lua, ptr: *mut c_void| {
-                    let value = unsafe { (*ptr.cast::<c_char>()).into_lua(lua)? };
-                    Ok(value)
-                },
-            )?
-            .into_lua(lua)?,
-        ),
-    ])
 }
