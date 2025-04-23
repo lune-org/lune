ad8822c1e786d6243b54a8dc9a80115f49471077
Filip Tibell <filip.tibell@gmail.com>
2025-04-23 13:21:25 +0200

    Remove re-export of lune roblox crate in main crate

--- crates/lune/src/lib.rs
+++ crates/lune/src/lib.rs
@@ -2,10 +2,6 @@
 
 mod rt;
 
-// TODO: Remove this in 0.9.0 since it is now available as a separate crate!
-#[cfg(feature = "std-roblox")]
-pub use lune_roblox as roblox;
-
 #[cfg(test)]
 mod tests;
 
