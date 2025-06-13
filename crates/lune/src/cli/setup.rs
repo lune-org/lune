1a8dd789745c778113ac576e30d9f4bd6dfd740f
Filip Tibell <filip.tibell@gmail.com>
2025-06-13 12:30:28 +0200

    Update setup cli command to use luaurc instead of legacy vscode-specific settings

--- crates/lune/src/cli/setup.rs
+++ crates/lune/src/cli/setup.rs
@@ -1,4 +1,4 @@
-use std::{borrow::BorrowMut, env::current_dir, io::ErrorKind, path::PathBuf, process::ExitCode};
+use std::{io::ErrorKind, process::ExitCode};
 
 use anyhow::{Context, Result};
 use async_fs as fs;
@@ -6,11 +6,9 @@ use clap::Parser;
 use directories::UserDirs;
 use thiserror::Error;
 
-// TODO: Use a library that supports json with comments since VSCode settings may contain comments
 use serde_json::Value as JsonValue;
 
-pub(crate) static SETTING_NAME_MODE: &str = "luau-lsp.require.mode";
-pub(crate) static SETTING_NAME_ALIASES: &str = "luau-lsp.require.directoryAliases";
+const LUAURC_PATH: &str = ".luaurc";
 
 /// Set up type definitions for your editor
 #[derive(Debug, Clone, Parser)]
@@ -22,29 +20,16 @@ impl SetupCommand {
             .await
             .expect("Failed to generate typedef files");
 
-        // TODO: Let the user interactively choose what editor to set up
-        let res = async {
-            let settings = read_or_create_vscode_settings_json().await?;
-            let modified = add_values_to_vscode_settings_json(settings);
-            write_vscode_settings_json(modified).await?;
-            Ok::<_, SetupError>(())
-        }
-        .await;
+        let mut luaurc = read_or_create_luaurc().await?;
 
-        let message = match res {
-            Ok(()) => "These settings have been added to your workspace for Visual Studio Code:",
-            Err(_) => "To finish setting up your editor, add these settings to your workspace:",
-        };
+        add_values_to_luaurc(&mut luaurc);
 
-        let version_string = lune_version();
+        write_luaurc(luaurc).await?;
+
         println!(
-            "Lune has now been set up and editor type definitions have been generated.\
-            \n{message}\
-            \n\
-            \n\"{SETTING_NAME_MODE}\": \"relativeToFile\",\
-            \n\"{SETTING_NAME_ALIASES}\": {{\
-            \n    \"@lune/\": \"~/.lune/.typedefs/{version_string}/\"\
-            \n}}",
+            "Type definitions for Lune v{} have been set up successfully.\
+            \nYou may need to restart your editor for the changes to take effect.",
+            lune_version()
         );
 
         Ok(ExitCode::SUCCESS)
@@ -67,29 +52,12 @@ fn lune_version() -> &'static str {
     env!("CARGO_PKG_VERSION")
 }
 
-fn vscode_path() -> PathBuf {
-    current_dir()
-        .expect("No current dir")
-        .join(".vscode")
-        .join("settings.json")
-}
-
-async fn read_or_create_vscode_settings_json() -> Result<JsonValue, SetupError> {
-    let path_file = vscode_path();
-    let mut path_dir = path_file.clone();
-    path_dir.pop();
-    match fs::read(&path_file).await {
-        Err(e) if e.kind() == ErrorKind::NotFound => {
-            // TODO: Make sure that VSCode is actually installed, or
-            // let the user choose their editor for interactive setup
-            match fs::create_dir_all(path_dir).await {
-                Err(_) => Err(SetupError::Write),
-                Ok(()) => match fs::write(path_file, "{}").await {
-                    Err(_) => Err(SetupError::Write),
-                    Ok(()) => Ok(JsonValue::Object(serde_json::Map::new())),
-                },
-            }
-        }
+async fn read_or_create_luaurc() -> Result<JsonValue, SetupError> {
+    match fs::read(LUAURC_PATH).await {
+        Err(e) if e.kind() == ErrorKind::NotFound => match fs::write(LUAURC_PATH, "{}").await {
+            Err(_) => Err(SetupError::Write),
+            Ok(()) => Ok(JsonValue::Object(serde_json::Map::new())),
+        },
         Err(_) => Err(SetupError::Read),
         Ok(contents) => match serde_json::from_slice(&contents) {
             Err(_) => Err(SetupError::Deserialize),
@@ -98,41 +66,39 @@ async fn read_or_create_vscode_settings_json() -> Result<JsonValue, SetupError> 
     }
 }
 
-async fn write_vscode_settings_json(value: JsonValue) -> Result<(), SetupError> {
-    match serde_json::to_vec_pretty(&value) {
+async fn write_luaurc(luaurc: JsonValue) -> Result<(), SetupError> {
+    match serde_json::to_vec_pretty(&luaurc) {
         Err(_) => Err(SetupError::Serialize),
-        Ok(json) => match fs::write(vscode_path(), json).await {
-            Err(_) => Err(SetupError::Write),
-            Ok(()) => Ok(()),
-        },
+        Ok(mut json) => {
+            json.push(b'\n');
+            match fs::write(LUAURC_PATH, json).await {
+                Err(_) => Err(SetupError::Write),
+                Ok(()) => Ok(()),
+            }
+        }
     }
 }
 
-fn add_values_to_vscode_settings_json(value: JsonValue) -> JsonValue {
-    let mut settings_json = value;
-    if let JsonValue::Object(settings) = settings_json.borrow_mut() {
-        // Set require mode
-        let mode_val = "relativeToFile".to_string();
-        settings.insert(SETTING_NAME_MODE.to_string(), JsonValue::String(mode_val));
-        // Set require alias to our typedefs
-        let aliases_key = "@lune/".to_string();
-        let aliases_val = format!("~/.lune/.typedefs/{}/", lune_version());
-        if let Some(JsonValue::Object(aliases)) = settings.get_mut(SETTING_NAME_ALIASES) {
-            if aliases.contains_key(&aliases_key) {
-                if aliases.get(&aliases_key).unwrap() != &JsonValue::String(aliases_val.to_string())
-                {
-                    aliases.insert(aliases_key, JsonValue::String(aliases_val));
+fn add_values_to_luaurc(luaurc: &mut JsonValue) {
+    if let JsonValue::Object(luaurc) = luaurc {
+        let field = String::from("aliases");
+        let alias = String::from("lune");
+        let dir = JsonValue::String(format!("~/.lune/.typedefs/{}/", lune_version()));
+
+        if let Some(JsonValue::Object(aliases)) = luaurc.get_mut(&field) {
+            if aliases.contains_key(&alias) {
+                if aliases.get(&alias).unwrap() != &dir {
+                    aliases.insert(alias, dir);
                 }
             } else {
-                aliases.insert(aliases_key, JsonValue::String(aliases_val));
+                aliases.insert(alias, dir);
             }
         } else {
             let mut map = serde_json::Map::new();
-            map.insert(aliases_key, JsonValue::String(aliases_val));
-            settings.insert(SETTING_NAME_ALIASES.to_string(), JsonValue::Object(map));
+            map.insert(alias, dir);
+            luaurc.insert(field, JsonValue::Object(map));
         }
     }
-    settings_json
 }
 
 async fn generate_typedef_files_from_definitions() -> Result<String> {
